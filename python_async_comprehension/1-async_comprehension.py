0299290f933e78b164ea0320a5d76f7cc982fc87
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-04-09 08:22:16 +0000

    Fix: Adjust docstring formatting to satisfy pycodestyle and checker

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""This module defines a coroutine that collects values from an async generator."""
+"""Module that defines a coroutine using async comprehension."""
 
 from typing import List
 
@@ -7,5 +7,9 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Return a list of 10 random floats collected using async comprehension."""
-    return [value async for value in async_generator()]
+    """Collect 10 random floats using async comprehension.
+
+    Returns:
+        List[float]: list of 10 random float values
+    """
+    return [i async for i in async_generator()]
