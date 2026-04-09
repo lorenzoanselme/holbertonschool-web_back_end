e45b8c115e6bfb00970da3741e77d63880b376ac
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-04-09 08:06:51 +0000

    Fix: Add return type annotation to async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """Yield 10 random numbers between 0 and 10 asynchronously.
 
     Yields:
