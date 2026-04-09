7906e5e4d0a531fd8720aa6da71c76fa79a14fa9
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-04-09 07:33:18 +0000

    Feat: Add a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module that provides a function to concatenate two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of two strings.
+
+    Args:
+        str1 (str): first string
+        str2 (str): second string
+
+    Returns:
+        str: concatenated string
+    """
+    return str1 + str2
