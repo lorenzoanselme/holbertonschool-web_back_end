0e515b9d9e447a0e704bf08074f3d9562811e28f
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-04-09 07:27:34 +0000

    Fix : Add documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+"""Module that provides a function to add two floats."""
+
 
 def add(a: float, b: float) -> float:
+    """Return the sum of two floats.
+
+    Args:
+        a (float): first number
+        b (float): second number
+
+    Returns:
+        float: sum of a and b
+    """
     return a + b
