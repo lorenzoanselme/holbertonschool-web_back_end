a536a3625548c53d411e8644a57c5a5ce2d4a14e
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-04-20 07:31:37 +0000

    Feat: Add simple helper function for pagination and README

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Simple helper function for pagination"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Return start and end index for pagination"""
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
